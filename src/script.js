d57e1c8111ffd5b4cc5b2a71fdd311348d244768
Carlos Augusto dos Reis <hadqen@gmail.com>
2023-01-05 18:34:52 -0300

    refactor: refatora as funções que adicionam uma nova tarefa

--- src/script.js
+++ src/script.js
@@ -1,72 +1,75 @@
-//adiciona nova tarefa
-let inputTexto = document.getElementById('texto-tarefa');
-inputTexto.addEventListener('keyup', novaTarefa);
-let tarefa;
+//add new task
+const button = document.getElementById('create-task');
+button.addEventListener('click', createTask);
+const taskList = document.getElementById('task-list');
 
-let botao = document.getElementById('criar-tarefa');
-botao.addEventListener('click', criaTarefa);
-let listaTarefas = document.getElementById('lista-tarefas');
-
-
-function novaTarefa(evento){
-    tarefa = evento.target.value;
+function getTask() {
+  const task = document.getElementById('task-text').value;
+  return task;
 }
 
-function criaTarefa(evento){
-    let tarefaItem = document.createElement('li');
-    tarefaItem.innerText = tarefa;
-    tarefaItem.addEventListener('click', alterarCor);
-    tarefaItem.addEventListener('dblclick', itemCompleto);
-    tarefaDeletada.push(tarefaItem);
-    listaTarefas.appendChild(tarefaItem);
-    inputTexto.value = '';
+function createTask() {
+  const newTask = getTask();
+
+  if (newTask !== '') {
+    const taskItem = document.createElement('li');
+    taskItem.innerText = newTask;
+    taskItem.addEventListener('click', alterarCor);
+    taskItem.addEventListener('dblclick', itemCompleto);
+
+    taskList.appendChild(taskItem);
+  }
+
+  document.getElementById('task-text').value = ''
 }
 
 //muda a cor da tarefa selecionada(adicionar o evento na criação do item)
 let tarefaSelecionada = document.getElementsByTagName('li');
 
-function alterarCor(evento){
-    for(let index = 0; index < tarefaSelecionada.length; index += 1){
-        if(tarefaSelecionada[index].style.backgroundColor === 'grey'){
-            tarefaSelecionada[index].style.backgroundColor = 'white';
-        }
+function alterarCor(evento) {
+  for (let index = 0; index < tarefaSelecionada.length; index += 1) {
+    if (tarefaSelecionada[index].style.backgroundColor === 'grey') {
+      tarefaSelecionada[index].style.backgroundColor = 'white';
     }
-    evento.target.style.backgroundColor = 'grey';
+  }
+  evento.target.style.backgroundColor = 'grey';
 }
 
 //item completo
-function itemCompleto(evento){
-    console.log(evento.target);
-    if(evento.target.className === 'completed'){
-        evento.target.className = '';
-    }else {
-        evento.target.className = 'completed';
-    }
+function itemCompleto(evento) {
+  console.log(evento.target);
+  if (evento.target.className === 'completed') {
+    evento.target.className = '';
+  } else {
+    evento.target.className = 'completed';
+  }
 }
 
 //apagar tarefas
-let botaoApagar = document.getElementById('apaga-tudo');
+let botaoApagar = document.getElementById('delete-tasks');
 botaoApagar.addEventListener('click', apagaTarefas);
 let tarefaDeletada = [];
 
-function apagaTarefas(evento){
-    console.log(tarefaDeletada);
-    for(let index = 0; index < tarefaDeletada.length; index += 1){
-        document.getElementById('lista-tarefas').removeChild(tarefaDeletada[index]);
-    }
-    tarefaDeletada = [];
+function apagaTarefas(evento) {
+  console.log(tarefaDeletada);
+  for (let index = 0; index < tarefaDeletada.length; index += 1) {
+    document.getElementById('task-list').removeChild(tarefaDeletada[index]);
+  }
+  tarefaDeletada = [];
 }
 
 //remove tarefas finalizadas
-let botaoFinalizado = document.getElementById('remover-finalizados');
+let botaoFinalizado = document.getElementById('delete-finalized');
 botaoFinalizado.addEventListener('click', apagaFinalizadas);
 
-function apagaFinalizadas(evento){
-    console.log(tarefaDeletada);
-    for(let index = tarefaDeletada.length -1; index >= 0; index -= 1){
-        if(tarefaDeletada[index].className === 'completed'){
-            document.getElementById('lista-tarefas').removeChild(tarefaDeletada[index]);
-            tarefaDeletada.splice(index, 1);
-        }
+function apagaFinalizadas(evento) {
+  console.log(tarefaDeletada);
+  for (let index = tarefaDeletada.length - 1; index >= 0; index -= 1) {
+    if (tarefaDeletada[index].className === 'completed') {
+      document
+        .getElementById('task-list')
+        .removeChild(tarefaDeletada[index]);
+      tarefaDeletada.splice(index, 1);
     }
+  }
 }

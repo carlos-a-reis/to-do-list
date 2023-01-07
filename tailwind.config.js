f392864df2964524cc8581efbf117ea29253a98e
Carlos Augusto dos Reis <hadqen@gmail.com>
2023-01-07 14:06:36 -0300

    feat: adiciona novas corres e tamanhos extendendo o tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['**/*.{html, js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-pastel': '#CEEDC7',
+        'green-pastel-drak': '#86C8BC',
+      },
+      fontSize: {
+        '2.5rem': '2.5rem',
+      },
+      spacing: {
+        '328px': '328px',
+      },
+    },
   },
   plugins: [],
 };

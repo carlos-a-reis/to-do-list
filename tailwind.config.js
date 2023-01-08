0e656f17fc8a3e1abe8f9446ee913698af284670
Carlos Augusto dos Reis <hadqen@gmail.com>
2023-01-07 22:42:05 -0300

    feat: adiciona a função de dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['**/*.{html, js}'],
   theme: {
     extend: {
       colors: {
         'green-pastel': '#CEEDC7',
         'green-pastel-drak': '#86C8BC',
+        'dark-blue': '#0A2647',
+        'light-blue': '#144272',
       },
       fontSize: {
         '2.5rem': '2.5rem',
